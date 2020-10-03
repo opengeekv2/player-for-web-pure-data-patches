f68974723d63c20ca42b595d00159bc4ebd8bd0e
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2020-10-03 19:24:27 +0200

    Add plugin basics

---
+++ wordpress-web-pure-data.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin entrypoint
+ *
+ * @package WordPress
+ *
+ * Plugin Name:       WordPress Web Pure Data
+ * Plugin URI:        https://github.com/opengeekv2/wordpress-web-pure-data
+ * Description:       Plugin to upload, show and run Web Pure Data compatible Pure Data programs in Wordpress sites.
+ * Version:           0.0.1
+ * Requires at least: 5.5
+ * Requires PHP:      5.3
+ * Author:            Marc Mauri ALloza
+ * Author URI:        https://github.com/opengeekv2
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ * Text Domain:       wpwpd
+ * Domain Path:       /languages
+ */
