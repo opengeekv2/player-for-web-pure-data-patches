d4861d31d7683da305ad4f479358f616d8f7f135
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2020-10-04 20:28:23 +0200

    Fix autoloade

--- tests/test-wordpress-web-pure-data.php
+++ tests/test-wordpress-web-pure-data.php
@@ -5,19 +5,19 @@
  * @package Wordpress_Web_Pure_Data
  */
 
-use WordpressWebPureData\WordpressWebPureData;
+use WPWPD\WordPress_Web_Pure_Data;
 
 /**
  * WordPress Web Pure Data Test.
  */
-class WordPressWebPureDataTest extends WP_UnitTestCase {
+class WordPress_Web_Pure_Data_Test extends WP_UnitTestCase {
 
 	/**
 	 * Test add Pure Data Mime Type.
 	 */
 	public function test_add_pd_mime_type() {
 		$mime_types['html']      = 'text/html';
-		$wordpress_web_pure_data = new WordpressWebPureData();
+		$wordpress_web_pure_data = new WordPress_Web_Pure_Data();
 
 		$mime_types = $wordpress_web_pure_data->add_pd_mime_type( $mime_types );
 
