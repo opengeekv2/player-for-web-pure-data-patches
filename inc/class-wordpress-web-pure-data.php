8d3ff1017da22518d1903c966ebd22a1d2d04416
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2020-10-04 22:23:48 +0200

    Prototype

--- inc/class-wordpress-web-pure-data.php
+++ inc/class-wordpress-web-pure-data.php
@@ -18,7 +18,9 @@ class WordPress_Web_Pure_Data {
 	 * Construct
 	 */
 	public function __construct() {
+		wp_register_script( 'webpd', plugin_dir_url( __DIR__ ) . 'assets/js/webpd-latest.min.js', array(), '0.3.1', true );
 		add_filter( 'upload_mimes', array( $this, 'add_pd_mime_type' ) );
+		add_shortcode( 'pd', array( $this, 'pd_shortcode_render' ) );
 	}
 
 	/**
@@ -32,6 +34,46 @@ class WordPress_Web_Pure_Data {
 		$mime_types['pd'] = 'text/plain';
 
 		return $mime_types;
+	}
+
+	/**
+	 * The [pd] shortcode.  Accepts a pd file path and will ru the PureData patch with Web Pure Data.
+	 *
+	 * @param string[] $atts     Shortcode attributes. Default empty.
+	 * @param string   $content  Shortcode content. Default null.
+	 * @param string   $tag      Shortcode tag (name). Default empty.
+	 *
+	 * @return string
+	 */
+	public function pd_shortcode_render( $atts = array(), $content = null, $tag = '' ) {
+		$atts = array_change_key_case( (array) $atts, CASE_LOWER );
+
+		/**
+		 * Var pd_atts type
+		 *
+		 * @var string[]
+		 */
+		$pd_atts = shortcode_atts(
+			array(
+				'patch' => '',
+			),
+			$atts,
+			$tag
+		);
+
+		wp_enqueue_script( 'webpd' );
+		$output = '<script>window.addEventListener("DOMContentLoaded", function () { fetch("' . $pd_atts['patch'] . '").then(function (response) { return response.text(); }).then(function (data) { var patch = Pd.loadPatch(data); Pd.start(); }); }, false);</script>';
+
+		// enclosing tags.
+		if ( ! is_null( $content ) ) {
+			// secure output by executing the_content filter hook on $content.
+			$output .= apply_filters( 'the_content', $content ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
+
+			// run shortcode parser recursively.
+			$output .= do_shortcode( $content );
+		}
+
+		return $output;
 	}
 
 }
